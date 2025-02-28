0837d7cc6be17df6255f43e85a1df8258041f25c
Thoogs <thoogs@pm.me>
2025-02-28 16:53:38 +0200

    format and lint code

--- library/book.py
+++ library/book.py
@@ -1,10 +1,12 @@
 """module handles the book objects and their sorting when needed."""
+
 from dataclasses import dataclass
 
 
 @dataclass
 class Book:
     """Contains data on books"""
+
     book_name: str
     author_name: str
     isbn: int
@@ -21,4 +23,3 @@ def sort_books(books: list[Book]) -> list:
     """Sorts a list of book objects by publication year"""
     books_sorted = [book for book in sorted(books, key=lambda book: book.pub_year)]
     return books_sorted
-
--- test/test_book.py
+++ test/test_book.py
@@ -2,22 +2,24 @@ import pytest
 
 from library.book import Book, sort_books
 
+
 @pytest.fixture
 def creare_test_list_of_books():
     return [
         Book("Moby Dick", "Herman Melville", 9781974305032, 1981),
-        Book("Idiot", "Fyodor Dostoyevsky", 9780850670356, 1971)
+        Book("Idiot", "Fyodor Dostoyevsky", 9780850670356, 1971),
     ]
+
 
 def test_book():
     book = Book("Idiot", "Fyodor Dostoyevsky", 9780850670356, 1971)
     expected = "Book(book_name='Idiot', author_name='Fyodor Dostoyevsky', isbn=9780850670356, pub_year=1971)"
     assert str(book) == expected
 
+
 def test_sort_books(creare_test_list_of_books):
     expected = [
         Book("Idiot", "Fyodor Dostoyevsky", 9780850670356, 1971),
-        Book("Moby Dick", "Herman Melville", 9781974305032, 1981)
+        Book("Moby Dick", "Herman Melville", 9781974305032, 1981),
     ]
     assert sort_books(creare_test_list_of_books) == expected
-
