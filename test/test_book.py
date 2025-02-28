916dc1772d9292da0b126bcff25dd23614bdc9d2
Thoogs <thoogs@pm.me>
2025-02-28 21:46:37 +0200

    Add unittests for csv_to_books and books_to_csv

--- test/test_book.py
+++ test/test_book.py
@@ -1,6 +1,6 @@
 import pytest
 
-from library.book import Book, sort_books
+from library.book import Book, sort_books, csv_to_books, books_to_csv
 
 
 @pytest.fixture
@@ -9,6 +9,14 @@ def creare_test_list_of_books():
         Book("Moby Dick", "Herman Melville", 9781974305032, 1981),
         Book("Idiot", "Fyodor Dostoyevsky", 9780850670356, 1971),
     ]
+
+
+@pytest.fixture
+def create_csv():
+    return """ 
+        Moby Dick/Herman Melville/9781974305032/1981
+        Idiot/Fyodor Dostoyevsky/9780850670356/1971
+    """.strip()
 
 
 def test_book():
@@ -23,3 +31,20 @@ def test_sort_books(creare_test_list_of_books):
         Book("Moby Dick", "Herman Melville", 9781974305032, 1981),
     ]
     assert sort_books(creare_test_list_of_books) == expected
+
+
+def test_csv_to_books(create_csv):
+    expected = [
+        Book("Moby Dick", "Herman Melville", 9781974305032, 1981),
+        Book("Idiot", "Fyodor Dostoyevsky", 9780850670356, 1971),
+    ]
+    assert csv_to_books(create_csv) == expected
+
+
+def test_books_to_csv(creare_test_list_of_books):
+    expected = """
+Moby Dick/Herman Melville/9781974305032/1981
+Idiot/Fyodor Dostoyevsky/9780850670356/1971
+""".lstrip()
+
+    assert books_to_csv(creare_test_list_of_books) == expected
